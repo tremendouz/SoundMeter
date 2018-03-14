b797cd5172c737591d44783a2da7f94883191cd0
dawid <zawrotny@protonmail.com>
2018-03-14 17:30:26 +0100

    Add layouts for login and register activity

--- app/src/main/java/com/example/dawid/soundmeter/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/dawid/soundmeter/di/ApplicationComponent.kt
@@ -3,6 +3,7 @@ package com.example.dawid.soundmeter.di
 import com.example.dawid.soundmeter.MainActivity
 import com.example.dawid.soundmeter.di.modules.AndroidModule
 import dagger.Component
+import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
 /**
@@ -10,7 +11,8 @@ import javax.inject.Singleton
  */
 
 @Singleton
-@Component(modules = arrayOf(AndroidModule::class))
+@Component(modules = arrayOf(AndroidModule::class,
+        AndroidSupportInjectionModule::class))
 interface ApplicationComponent {
     fun inject(activity: MainActivity)
 }
